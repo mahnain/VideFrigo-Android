3d5293af1ad096d5966dd6203609453685c45659
Jalal AH <202197893@collegeahuntsic.qc.ca>
2022-09-28 17:46:55 -0400

    Added Google Sign in with Goodle Button Sign In at the LoginActivity

--- app/src/main/java/jalal/android/authenticationtest/LoginActivity.kt
+++ app/src/main/java/jalal/android/authenticationtest/LoginActivity.kt
@@ -1,5 +1,6 @@
 package jalal.android.authenticationtest
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,27 +9,55 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 private const val TAG = "LoginActivity"
-private const val REQUEST_CODE = 5555;
+//private const val REQUEST_CODE = 5555;
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         val etEmail = findViewById<EditText>(R.id.etEmail)
         val etPassword = findViewById<EditText>(R.id.etPassword)
         val etNotRegister = findViewById<TextView>(R.id.etNotRegister)
+        val gSignInBtn = findViewById<Button>(R.id.gSignInBtn)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this , gso)
+/*
+        findViewById<Button>(R.id.gSignInBtn).setOnClickListener {
+           // signInGoogle()
+        }
+*/
         etNotRegister.setOnClickListener {
 
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
-        val auth = FirebaseAuth.getInstance()
+        gSignInBtn.setOnClickListener {
+             signInGoogle()
+        }
+
         if(auth.currentUser != null){
             goPostsActivity()
         }
@@ -65,6 +94,45 @@ class LoginActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                val intent : Intent = Intent(this , PostsActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
 
     private fun goPostsActivity() {
         Log.i(TAG, "goPostsActivity")
